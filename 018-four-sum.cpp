26d64604cce9d5252837d4b7a28a2144eec8924b
wyf2552 <wyf5322@gmail.com>
2025-01-20 13:04:52 +0800

    四和

--- 018-four-sum.cpp
+++ 018-four-sum.cpp
@@ -3,7 +3,21 @@
 
 using namespace std;
 
+/*
+给你一个由 n 个整数组成的数组 nums ，和一个目标值 target 。请你找出并返回满足下述全部条件且不重复的四元组 [nums[a], nums[b], nums[c], nums[d]] （若两个四元组元素一一对应，则认为两个四元组重复）：
+0 <= a, b, c, d < n
+a、b、c 和 d 互不相同
+nums[a] + nums[b] + nums[c] + nums[d] == target
+你可以按 任意顺序 返回答案 。
 
+示例 1：
+输入：nums = [1,0,-1,0,-2,2], target = 0
+输出：[[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
+示例 2：
+输入：nums = [2,2,2,2,2], target = 8
+输出：[[2,2,2,2]]
+*/
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
@@ -15,8 +29,18 @@ public:
 
         sort(nums.begin(), nums.end());
 
+        int length = nums.size();
+
         for (int i = 0; i < nums.size() - 3; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            if ((long)nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break;
+            }
+
+            if ((long)nums[i] + nums[length - 3] + nums[length - 2] + nums[ length - 1] < target) {
                 continue;
             }
 
@@ -25,25 +49,38 @@ public:
                     continue;
                 }
 
+                if ((long)nums[i] + nums[j] + nums[j + 1] + nums[j + 2]> target) {
+                    break;
+                }
+
+                if ((long)nums[i] + nums[j] + nums[length - 1] + nums[length - 2] < target) {
+                    continue;
+                }
                 int left = j + 1;
-                int right = nums.size() - 1;
+                int right = length - 1;
 
                 while (left < right) {
-                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
 
                     if (sum == target) {
                         result.push_back({nums[i], nums[j], nums[left], nums[right]});
-
                         while (left < right && nums[left] == nums[left + 1]) {
                             left++;
                         }
                         while (left < right && nums[right] == nums[right - 1]) {
                             right--;
                         }
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
                     }
+
                 }
             }
         }
         return result;
     }
 };
+
