4a3cb95d764213b6db3a9ca11ef36d45f944e3e0
wyf2552 <wyf5322@gmail.com>
2025-01-22 09:08:02 +0800

    公共前缀

---
+++ 014-longest-common-prefix.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+/*
+编写一个函数来查找字符串数组中的最长公共前缀。
+如果不存在公共前缀，返回空字符串 "。
+示例 1：
+输入：strs = ["flower","flow","flight"]
+输出："fl"
+示例 2：
+输入：strs = ["dog","racecar","car"]
+输出：""
+解释：输入不存在公共前缀。
+*/
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) {
+            return "";
+        }
+
+        string prefix = strs[0];
+
+        for (int i = 0; i < strs.size(); i++) {
+            while (strs[i].substr(0, prefix.length()) != prefix) {
+                prefix = prefix.substr(0, prefix.length() - 1);
+
+                if (prefix.empty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+};
