05df7fd10e90cb55f5e7c482ba9788a1050d9b71
wyf2552 <wyf5322@gmail.com>
2025-01-23 09:19:22 +0800

    矩阵区域和

---
+++ 1314-matrix-block-sum.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        vector<vector<int>> preSum(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                preSum[i][j] = preSum[i-1][j] + preSum[i][j-1] - preSum[i-1][j-1] + mat[i-1][j-1];
+            }
+        }
+
+        vector<vector<int>> answer(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int r1 = max(0, i - k);
+                int c1 = max(0, j - k);
+                int r2 = min(m - 1, i + k);
+                int c2 = min(n - 1, j + k);
+
+                answer[i][j] = preSum[r2 + 1][c2 + 1] - preSum[r2 + 1][c1]
+                              - preSum[r1][c2 + 1] + preSum[r1][c1];
+            }
+        }
+
+        return answer;
+    }
+};
