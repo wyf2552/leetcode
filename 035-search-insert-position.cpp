677767b3c5abb079beffe4af7dbc29fbc49e2143
wyf2552 <wyf5322@gmail.com>
2025-03-09 23:27:23 +0800

    搜索插入位置

---
+++ 035-search-insert-position.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+/*
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+请必须使用时间复杂度为 O(log n) 的算法。
+
+示例 1:
+输入: nums = [1,3,5,6], target = 5
+输出: 2
+
+示例 2:
+输入: nums = [1,3,5,6], target = 2
+输出: 1
+
+示例 3:
+输入: nums = [1,3,5,6], target = 7
+输出: 4
+*/
+class Solution {
+public:
+   int searchInsert(vector<int>& nums, int target) {
+     int left = 0;
+     int right = nums.size();
+
+     while (left < right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+     }
+     return left;
+   }
+};
