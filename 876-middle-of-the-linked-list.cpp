4af06c142448308bb8e6a2270d917780ae194843
wyf2552 <wyf5322@gmail.com>
2025-03-23 23:26:58 +0800

    链表的中间节点

---
+++ 876-middle-of-the-linked-list.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+/*
+给你单链表的头结点 head ，请你找出并返回链表的中间结点。
+如果有两个中间结点，则返回第二个中间结点。
+
+示例 1：
+输入：head = [1,2,3,4,5]
+输出：[3,4,5]
+解释：链表只有一个中间结点，值为 3 。
+
+示例 2：
+输入：head = [1,2,3,4,5,6]
+输出：[4,5,6]
+解释：该链表有两个中间结点，值分别为 3 和 4 ，返回第二个结点。
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;       // 慢指针每次走一步
+            fast = fast->next->next; // 快指针每次走两步
+        }
+
+        return slow;
+    }
+};
