19c5eec3a032235b22346d409c6f27b52ffa1bb2
wyf2552 <wyf5322@gmail.com>
2025-08-12 23:51:50 +0800

    从根到叶的二进制数之和

---
+++ 1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -0,0 +1,50 @@
+/*
+给出一棵二叉树，其上每个结点的值都是 0 或 1 。每一条从根到叶的路径都代表一个从最高有效位开始的二进制数。
+
+例如，如果路径为 0 -> 1 -> 1 -> 0 -> 1，那么它表示二进制数 01101，也就是 13 。
+对树上的每一片叶子，我们都要找出从根到该叶子的路径所表示的数字。
+
+返回这些数字之和。题目数据保证答案是一个 32 位 整数。
+
+示例 1：
+输入：root = [1,0,1,0,1,0,1]
+输出：22
+解释：(100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+
+示例 2：
+输入：root = [0]
+输出：0
+*/
+
+class Solution {
+private:
+    int path = 0;
+    int res = 0;
+
+    void traverse(TreeNode* root) {
+        if (root == nullptr) {
+            return;
+        }
+        if (root->left == nullptr && root->right == nullptr) {
+            res += path << 1 | root->val;
+            return;
+        }
+        path = (path << 1) | root->val;
+        traverse(root->left);
+        traverse(root->right);
+        path = path >> 1;
+    }
+
+public:
+    int sumRootToLeaf(TreeNode* root) {
+        traverse(root);
+        return res;
+    }
+};
+
+     1
+   0   1
+  0 1 0 1
+
+path=0
+res=22
