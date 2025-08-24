3a4b4648237214bf6b9ecb0519ce1f0d1b0c51e4
wyf2552 <wyf5322@gmail.com>
2025-08-24 14:20:50 +0800

    统计二叉树中好节点的树木

---
+++ 1448-count-good-nodes-in-binary-tree.cpp
@@ -0,0 +1,51 @@
+/*
+给你一棵根为 root 的二叉树，请你返回二叉树中好节点的数目。
+
+「好节点」X 定义为：从根到该节点 X 所经过的节点中，没有任何节点的值大于 X 的值。
+
+示例 1：
+输入：root = [3,1,4,3,null,1,5]
+输出：4
+解释：图中蓝色节点为好节点。
+根节点 (3) 永远是个好节点。
+节点 4 -> (3,4) 是路径中的最大值。
+节点 5 -> (3,4,5) 是路径中的最大值。
+节点 3 -> (3,1,3) 是路径中的最大值。
+
+示例 2：
+输入：root = [3,3,null,4,2]
+输出：3
+解释：节点 2 -> (3, 3, 2) 不是好节点，因为 "3" 比它大。
+
+示例 3：
+输入：root = [1]
+输出：1
+解释：根节点是好节点。
+*/
+
+class Solution {
+private:
+    int count = 0;
+
+    void traverse(TreeNode* root, int pathMax) {
+        if (root == nullptr) {
+            return;
+        }
+        if (pathMax <= root->val) {
+            count++;
+        }
+        pathMax = max(pathMax, root->val);
+
+        traverse(root->left, pathMax);
+        traverse(root->right, pathMax);
+    }
+public:
+    int goodNodes(TreeNode* root) {
+        traverse(root, root->val);
+        return count;
+    }
+};
+count=4 pathmax=5
+     3
+   1   4
+  3.  1 5
